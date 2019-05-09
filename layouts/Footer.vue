a95c578899813fb5522f1b7a2243b277deecf1a4
JessicaThi <jessica.thielemans@hetic.net>
2019-05-09 22:31:23 +0200

    update components footer css to scss + add scss tablet

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -9,11 +9,27 @@
       </form>
       <div class="contact__line-bottom"></div>
     </div>
+    <span class="line-vertical"></span>
     <div class="legal">
       <h3 class="legal__item section__title">Legal</h3>
-      <a class="legal__item legal__link" href="#">Mentions légales</a>
-      <a class="legal__item legal__link" href="#">Politiques de confidentialités</a>
-      <img class="legal__item legal__image" src="~assets/images/bobby-winking-footer.svg" alt>
+      <div class="legal__container-text">
+        <a class="legal__item legal__link" href="#">Mentions légales</a>
+        <a class="legal__item legal__link" href="#">Politiques de confidentialités</a>
+        <div class="legal__item legal__social-network">
+          <a href="#">
+            <img src="~assets/images/facebook.svg">
+          </a>
+          <a href="#">
+            <img src="~assets/images/instagram.svg">
+          </a>
+          <a href="#">
+            <img src="~assets/images/twitter.svg">
+          </a>
+        </div>
+      </div>
+      <div class="legal__container-image">
+        <img class="legal__image" src="~assets/images/bobby-winking-footer.svg" alt>
+      </div>
     </div>
     <p class="copyright">© Bob 2019</p>
   </footer>
@@ -22,94 +38,174 @@
 <script>
 export default {};
 </script>
-<style>
+<style lang="scss">
 footer {
-  background-color: #b3ecff;
+  background-color: $light_blue;
   padding: 30px 20px 10px;
+
+  @media #{$tablet} {
+    @include flexbox();
+    @include flex-wrap(wrap);
+  }
 }
 
-.contact__email {
-  margin-top: 20px;
-  height: 50px;
+.contact {
+  @media #{$tablet} {
+    width: 50%;
+  }
+
+  .section__title {
+    @media #{$tablet} {
+      text-align: left;
+    }
+  }
+
+  &__email {
+    margin-top: 20px;
+    height: 50px;
+  }
+
+  &__email,
+  &__textarea {
+    width: 100%;
+    max-width: 330px;
+    border-radius: 10px;
+    font-size: 16px;
+    border: 1px solid $light-grey;
+    padding-left: 15px;
+    outline: none;
+
+    &::placeholder {
+      color: $light-grey;
+      font-size: 16px;
+    }
+  }
+
+  &__textarea {
+    margin-top: 10px;
+    padding-top: 10px;
+  }
+
+  &__send-btn {
+    height: 50px;
+    width: 100%;
+    max-width: 330px;
+    background-color: $blue;
+    border-radius: 10px;
+    border: none;
+    color: $white;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 10px;
+  }
+
+  &__line-bottom {
+    display: block;
+    margin: 0 auto;
+    max-width: 375px;
+    width: 49%;
+    height: 1px;
+    background-color: $white;
+    margin-top: 30px;
+
+    @media #{$tablet} {
+      display: none;
+    }
+  }
 }
 
-.contact__email,
-.contact__textarea {
-  width: 100%;
-  max-width: 330px;
-  border-radius: 10px;
-  font-size: 16px;
-  border: 1px solid #cbd2d9;
-  padding-left: 15px;
-  outline: none;
-}
+.line-vertical {
+  display: none;
 
-.contact__email::placeholder,
-.contact__textarea::placeholder {
-  color: #cbd2d9;
-  font-size: 16px;
-}
-
-.contact__textarea {
-  margin-top: 10px;
-  padding-top: 10px;
-}
-
-.contact__send-btn {
-  height: 50px;
-  width: 100%;
-  max-width: 330px;
-  background-color: #40c3f7;
-  border-radius: 10px;
-  border: none;
-  color: #fff;
-  font-size: 18px;
-  font-weight: bold;
-  margin-top: 10px;
-}
-
-.contact__line-bottom {
-  display: block;
-  margin: 0 auto;
-  max-width: 375px;
-  width: 50%;
-  height: 1px;
-  background-color: #fff;
-  margin-top: 30px;
+  @media #{$tablet} {
+    display: inherit;
+    width: 1px;
+    height: 200px;
+    background-color: $white;
+    margin: auto;
+  }
 }
 
 .legal {
   padding-top: 30px;
   display: flex;
   flex-wrap: wrap;
-}
 
-.legal__item {
-  flex: 0 0 100%;
-}
+  @media #{$tablet} {
+    width: 49%;
+    padding-top: 0px;
+  }
 
-.legal .section__title {
-  padding-bottom: 15px;
-}
+  .section__title {
+    padding-bottom: 20px;
 
-.legal__link {
-  display: inline-block;
-  font-size: 16px;
-  color: #fff;
-  font-weight: bold;
-  text-decoration: none;
-  text-align: center;
-  padding-bottom: 15px;
-}
+    @media #{$tablet} {
+      text-align: left;
+    }
+  }
 
-.legal__image {
-  padding: 50px 0;
+  &__container-text {
+    @include flexbox();
+    @include flex-wrap(wrap);
+
+    @media #{$tablet} {
+      @include align-content(flex-start);
+      width: 60%;
+    }
+  }
+
+  &__item {
+    flex: 0 0 100%;
+  }
+
+  &__link {
+    display: inline-block;
+    font-size: 16px;
+    color: $white;
+    font-weight: bold;
+    text-decoration: none;
+    text-align: center;
+    padding-bottom: 15px;
+
+    @media #{$tablet} {
+      text-align: left;
+    }
+  }
+
+  &__social-network {
+    @include flexbox();
+    @include justify-content(space-evenly);
+    padding-top: 20px;
+  }
+
+  &__container-image {
+    margin: 0 auto;
+
+    @media #{$tablet} {
+      width: 40%;
+    }
+  }
+
+  &__image {
+    padding: 50px 0;
+
+    @media #{$tablet} {
+      padding: 0px;
+      display: block;
+      margin: 0 auto;
+    }
+  }
 }
 
 .copyright {
   font-size: 16px;
-  color: #fff;
+  color: $white;
   font-weight: normal;
   text-align: center;
+
+  @media #{$tablet} {
+    width: 100%;
+    padding-top: 30px;
+  }
 }
 </style>
